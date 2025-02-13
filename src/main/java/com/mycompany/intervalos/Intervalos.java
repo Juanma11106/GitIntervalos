af82d0c15c61450646fe1b9bf422cac6e4c6d200
juanma <juanma@gmail.com>
2025-02-13 18:53:00 +0000

    Primera version

---
+++ src/main/java/com/mycompany/intervalos/Intervalos.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.intervalos;
+
+/**
+ *
+ * @author juanma
+ */
+public class Intervalos {
+
+    public static void main(String[] args) {
+        
+        int a = 0;
+        String rango = Intervalos.ObtenerCategoria(a);
+        System.out.println("El número "+a+ "esta en el "+rango);
+        
+    }
+    
+    public static String ObtenerCategoria(int a){
+       if (a < -4 ){
+           final String A = "Intervalo A";
+           return A;
+       }
+       
+       if ( a>= -4 && a < 6){
+           return "Intervalo B";
+       }else{
+           return "Intervalo C";
+       }
+    }
+}
